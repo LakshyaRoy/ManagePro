c08c5a764240269bbc781f5193c6448de12e0169
lakshya <lakshyaroy848@gmail.com>
2024-06-11 01:48:29 +0530

    created a new context

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,11 +1,18 @@
 import React, { createContext, useState } from "react";
 
 export const Task = createContext();
+export const Filter = createContext();
 
 const Context = ({ children }) => {
   const [allItems, setAllItems] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+
   return (
-    <Task.Provider value={{ allItems, setAllItems }}>{children}</Task.Provider>
+    <Task.Provider value={{ allItems, setAllItems }}>
+      <Filter.Provider value={{ filteredItems, setFilteredItems }}>
+        {children}
+      </Filter.Provider>
+    </Task.Provider>
   );
 };
 
